628bd102c18dcc440217567fdf6154af40a0cd2c
shankar kumar <shankar.k@digitalboosts.com>
2026-09-08 23:11:27 +0530

    add code

--- cafemitra_server/api/views.py
+++ cafemitra_server/api/views.py
@@ -2629,7 +2629,7 @@ def check_server_status(request):
     if request.method == "OPTIONS":
         return JsonResponse({})
 
-    return JsonResponse({"status": "ok", "message": "Server is running version7."})
+    return JsonResponse({"status": "ok", "message": "Server is running version8."})
 
 
 @csrf_exempt
